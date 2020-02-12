f14e237e636904ba0f73761ba36d919ff4a5a42d
abhishekmca87 <42065100+abhishekmca87@users.noreply.github.com>
2020-02-12 13:02:51 +0530

    Add files via upload
    first commit

---
+++ GlobalAuthorizationControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using School.Model;
+
+namespace SchoolSoft
+{
+    public static class GlobalAuthorizationControl
+    {
+        public static Admin Auth { get; set; }
+    }
+}
